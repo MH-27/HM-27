b407ed423f049106018aa70691b1e24d4a3c1a8d
MH-27 <78807920+MH-27@users.noreply.github.com>
2021-02-21 11:04:38 +0300

    Update cleanx.py

--- cleanx.py
+++ cleanx.py
@@ -37,7 +37,7 @@ def cleanx(file):
         write.write(f"{x[num]}")
         write.close()
         num -= 1 
-    os.remove("not_clear")
+    os.remove("not_clear.txt")
     from termcolor import colored
     return (colored("      setup 6/18 is completed     ","red"))
 
