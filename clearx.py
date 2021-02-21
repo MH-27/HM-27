b15ff82a25e79cddbfcbe5feef706a8aa5781174
MH-27 <78807920+MH-27@users.noreply.github.com>
2021-02-21 11:03:18 +0300

    Update clearx.py

--- clearx.py
+++ clearx.py
@@ -29,8 +29,15 @@ def clearx(file):
     open1.close()
     creator.close()
     open2.close()
-    os.remove(file)
-    os.rename(f"not_clear.txt",file)
+    openx = open("not_clear.txt")
+    x = openx.readlines()
+    num = len(x)-1
+    for i in x :
+        write = open(file,"a")
+        write.write(f"{x[num]}")
+        write.close()
+        num -= 1 
+    os.remove("not_clear")
     from termcolor import colored
     return (colored("      setup 9/18 is completed     ","red"))
 
